e85215d56533370e8de074d2f950c606cc331264
snowdreams1006 <snowdreams1006@163.com>
2019-12-18 13:08:53 +0800

    rm assert

--- assets/plugin.js
+++
@@ -1,7 +0,0 @@
-require(["gitbook", "jQuery"], function(gitbook, $) {
-    gitbook.events.bind('start', function(e, config) {
-        var conf = config['icp'];
-        
-        console.log(conf);
-    });
-});
--- index.js
+++ index.js
@@ -2,40 +2,17 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = {
-	book: {
-        assets: "./assets",
-        js: ["plugin.js"]
-    },
     hooks: {
-        "init": function() {
-            this.log.debug.ln('init', this.options.pluginsConfig['icp']);
-        },
-        "finish:before": function() {
-            this.log.debug.ln('finish:before', this.options.pluginsConfig['icp']);
-        },
         "finish": function() {
             this.log.debug.ln('finish', this.options.pluginsConfig['icp']);
 
-            // var faviconPathFile = this.options.pluginsConfig['favicon-absolute'] && this.options.pluginsConfig['favicon-absolute'].favicon;
-            // if (faviconPathFile.startsWith("/")) {
-            //     faviconPathFile = faviconPathFile.substring(1, faviconPathFile.length);
-            // }
-            // var faviconAbsolutePath = path.join(process.cwd(), faviconPathFile);
-            // var gitbookFaviconPath = path.join(process.cwd(), '_book', 'gitbook', 'images', 'favicon.ico');
-            // if (faviconPathFile && fs.existsSync(faviconPathFile) && fs.existsSync(gitbookFaviconPath)) {
-            //     fs.unlinkSync(gitbookFaviconPath);
-            //     fs.createReadStream(faviconAbsolutePath).pipe(fs.createWriteStream(gitbookFaviconPath));
-            // }
+            var number = this.options.pluginsConfig['icp'] && this.options.pluginsConfig['icp'].number;
+            var indexPath = path.join(process.cwd(), '_book', 'index.html');
+            
+            console.log("number="+number+",indexPath="+indexPath);
 
-            // var appleTouchIconPrecomposed152PathFile = this.options.pluginsConfig['favicon-absolute'] && this.options.pluginsConfig['favicon-absolute'].appleTouchIconPrecomposed152;
-            // if (appleTouchIconPrecomposed152PathFile.startsWith("/")) {
-            //     appleTouchIconPrecomposed152PathFile = appleTouchIconPrecomposed152PathFile.substring(1, appleTouchIconPrecomposed152PathFile.length);
-            // }
-            // var appleTouchIconPrecomposed152 = path.join(process.cwd(), appleTouchIconPrecomposed152PathFile);
-            // var gitbookAppleTouchPath = path.join(process.cwd(), '_book', 'gitbook', 'images', 'apple-touch-icon-precomposed-152.png');
-            // if (appleTouchIconPrecomposed152PathFile && fs.existsSync(appleTouchIconPrecomposed152PathFile) && fs.existsSync(gitbookAppleTouchPath)) {
-            //     fs.unlinkSync(gitbookAppleTouchPath);
-            //     fs.createReadStream(appleTouchIconPrecomposed152).pipe(fs.createWriteStream(gitbookAppleTouchPath));
+            // if (number && fs.existsSync(indexPath)) {
+            //     fs.createReadStream(indexPath).pipe(fs.createWriteStream(indexPath));
             // }
         }
     }
