8e591f25fb343aaf8a337cc4c7c45275c4759291
snowdreams1006 <snowdreams1006@163.com>
2019-12-18 12:32:53 +0800

    add _layout

--- example/README.md
+++ example/README.md
@@ -228,3 +228,6 @@ This project is [MIT](https://github.com/snowdreams1006/gitbook-plugin-edit-link
 
 1. If you are not seeing the "Edit this page" link, check if your `book.json` is valid. You can use this online tool - [http://json.parser.online.fr/beta/](http://json.parser.online.fr/beta/)
 2. Check if you are using default gitbook theme. It is NOT recommended to modify gitbook themes directly.
+
+<div style="position: absolute;bottom: 0;left: 50%;transform: translate(-50%, -50%);">ICP证：<a href="http://beian.miit.gov.cn/" target="_blank" style="color:#f72b07">浙ICP备18042346号</a>
+</div>
--- index.js
+++ index.js
@@ -1,6 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
-    book: {
+	book: {
         assets: "./assets",
         js: ["plugin.js"]
+    },
+    hooks: {
+        "init": function() {
+            this.log.debug.ln('init', this.options.pluginsConfig['icp']);
+        },
+        "finish:before": function() {
+            this.log.debug.ln('finish:before', this.options.pluginsConfig['icp']);
+        },
+        "finish": function() {
+            this.log.debug.ln('finish', this.options.pluginsConfig['icp']);
+
+            // var faviconPathFile = this.options.pluginsConfig['favicon-absolute'] && this.options.pluginsConfig['favicon-absolute'].favicon;
+            // if (faviconPathFile.startsWith("/")) {
+            //     faviconPathFile = faviconPathFile.substring(1, faviconPathFile.length);
+            // }
+            // var faviconAbsolutePath = path.join(process.cwd(), faviconPathFile);
+            // var gitbookFaviconPath = path.join(process.cwd(), '_book', 'gitbook', 'images', 'favicon.ico');
+            // if (faviconPathFile && fs.existsSync(faviconPathFile) && fs.existsSync(gitbookFaviconPath)) {
+            //     fs.unlinkSync(gitbookFaviconPath);
+            //     fs.createReadStream(faviconAbsolutePath).pipe(fs.createWriteStream(gitbookFaviconPath));
+            // }
+
+            // var appleTouchIconPrecomposed152PathFile = this.options.pluginsConfig['favicon-absolute'] && this.options.pluginsConfig['favicon-absolute'].appleTouchIconPrecomposed152;
+            // if (appleTouchIconPrecomposed152PathFile.startsWith("/")) {
+            //     appleTouchIconPrecomposed152PathFile = appleTouchIconPrecomposed152PathFile.substring(1, appleTouchIconPrecomposed152PathFile.length);
+            // }
+            // var appleTouchIconPrecomposed152 = path.join(process.cwd(), appleTouchIconPrecomposed152PathFile);
+            // var gitbookAppleTouchPath = path.join(process.cwd(), '_book', 'gitbook', 'images', 'apple-touch-icon-precomposed-152.png');
+            // if (appleTouchIconPrecomposed152PathFile && fs.existsSync(appleTouchIconPrecomposed152PathFile) && fs.existsSync(gitbookAppleTouchPath)) {
+            //     fs.unlinkSync(gitbookAppleTouchPath);
+            //     fs.createReadStream(appleTouchIconPrecomposed152).pipe(fs.createWriteStream(gitbookAppleTouchPath));
+            // }
+        }
     }
 };
