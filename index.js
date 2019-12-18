9c6e4b52c8b145902d3e42107c7281c696545b08
snowdreams1006 <snowdreams1006@163.com>
2019-12-18 18:07:33 +0800

    add cheerio

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 var path = require('path');
+var cheerio = require('cheerio');
+
+
 
 module.exports = {
     hooks: {
@@ -8,12 +11,25 @@ module.exports = {
 
             var number = this.options.pluginsConfig['icp'] && this.options.pluginsConfig['icp'].number;
             var indexPath = path.join(process.cwd(), '_book', 'index.html');
-            
-            console.log("number="+number+",indexPath="+indexPath);
+            var multilingual = this.isLanguageBook();
 
-            // if (number && fs.existsSync(indexPath)) {
-            //     fs.createReadStream(indexPath).pipe(fs.createWriteStream(indexPath));
-            // }
+            console.log("number="+number+",indexPath="+indexPath+",multilingual="+multilingual);
+
+if (multilingual && number && fs.existsSync(indexPath)) {
+    var indexText = fs.readFileSync(indexPath);
+    indexText = indexText.toString();
+console.log(indexText);
+
+$ = cheerio.load(indexText);
+var str ='<div style="position: absolute;bottom: 0;left: 50%;transform: translate(-50%, -50%);">ICP证：<a href="http://beian.miit.gov.cn/" target="_blank" style="color:#f72b07">浙ICP备18042346号</a></div>';
+$('body').append(str);
+indexText = $.html();
+
+console.log(indexText);
+
+fs.writeFileSync(indexPath,indexText);
+
+            }
         }
     }
 };
